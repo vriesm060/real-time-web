30274292d84645627801b6bd0f7d48764778e735
Max de Vries <max.devries.1995@gmail.com>
2018-04-10 13:33:50 +0200

    Added socket.io

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var request = require('request');
 var bodyParser = require('body-parser');
-var app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -12,4 +14,27 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.listen(3000);
+/*
+
+  To do:
+  ______
+
+  * Show history of chat messages
+  * Show information about the users
+  *
+
+*/
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+  });
+});
+
+http.listen(3000);
---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+var socket = io();
+var m = document.getElementById('m');
+var messages = document.getElementById('messages');
+
+// (function () {
+//
+// }) ();
+
+m.parentNode.addEventListener('submit', function (e) {
+  socket.emit('chat message', m.value);
+  m.value = '';
+  e.preventDefault();
+}, false);
+
+socket.on('chat message', function(msg) {
+  var li = document.createElement('li');
+  li.textContent = msg.username + ': ' + msg.msg;
+  messages.appendChild(li);
+});
