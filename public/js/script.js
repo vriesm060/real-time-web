c15a0067fdeb2ac401bd559f3750a96e0f1d205e
Max de Vries <max.devries.1995@gmail.com>
2018-04-12 15:42:28 +0200

    Updated game

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,43 @@ checkbox.forEach(function (item) {
   }, false);
 });
 
-socket.on('add-move', function (move) {
-  document.getElementById(move.id).nextElementSibling.style.backgroundColor = move.player;
+socket.on('add-move', function (disc) {
+  connectFour.dropDisc(disc);
+  connectFour.checkRows();
 });
+
+var connectFour = {
+  selectedRows: [],
+  selectedCols: [],
+  curRow: function (id) {
+    return id.slice(1);
+  },
+  curCol: function (id) {
+    return id.slice(0, 1);
+  },
+  dropDisc: function (disc) {
+    var arr = [];
+
+    for (var i = 1; i < 7; i++) {
+      // If disc is already in place:
+      if (document.getElementById(this.curCol(disc.id) + i).disabled === false) {
+        arr.push(i);
+      }
+    }
+
+    var row = arr[0];
+    var col = this.curCol(disc.id);
+
+    this.selectedRows.push(row);
+    this.selectedCols.push(col);
+
+    document.getElementById(this.curCol(disc.id) + arr[0]).disabled = true;
+    document.getElementById(this.curCol(disc.id) + arr[0]).nextElementSibling.style.backgroundColor = disc.player;
+  },
+  checkRows: function () {
+    console.log(this.selectedRows);
+
+    
+
+  }
+};
