a22bed460fba2729f479dbb0b01a654753599490
Max de Vries <max.devries.1995@gmail.com>
2018-04-13 21:23:39 +0200

    Added diagonal win calculation

--- public/js/script.js
+++ public/js/script.js
@@ -41,15 +41,22 @@ var connectFour = {
     if (disc.player === 'red') {
       this.playerRedMoves.push(space);
       document.getElementById(space).nextElementSibling.style.backgroundColor = '#FF004C';
-      if (this.checkMovesHorizontal(space, this.playerRedMoves) >= 3 || this.checkMovesVertical(space, this.playerRedMoves) >= 3) {
-        console.log('Red won!');
+      this.checkMovesBottomLeftTopRight(space, this.playerRedMoves);
+      if (this.checkMovesHorizontal(space, this.playerRedMoves) >= 3 ||
+          this.checkMovesVertical(space, this.playerRedMoves) >= 3 ||
+          this.checkMovesBottomLeftTopRight(space, this.playerRedMoves) >= 3 ||
+          this.checkMovesBottomRightTopLeft(space, this.playerRedMoves) >= 3
+      ) {
         h1.textContent = 'Red won!';
       }
     } else {
       this.playerYellowMoves.push(space);
       document.getElementById(space).nextElementSibling.style.backgroundColor = '#FFEE19';
-      if (this.checkMovesHorizontal(space, this.playerYellowMoves) >= 3 || this.checkMovesVertical(space, this.playerYellowMoves) >= 3) {
-        console.log('Yellow won!');
+      if (this.checkMovesHorizontal(space, this.playerYellowMoves) >= 3 ||
+          this.checkMovesVertical(space, this.playerYellowMoves) >= 3 ||
+          this.checkMovesBottomLeftTopRight(space, this.playerYellowMoves) >= 3 ||
+          this.checkMovesBottomRightTopLeft(space, this.playerYellowMoves) >= 3
+      ) {
         h1.textContent = 'Yellow won!';
       }
     }
@@ -96,7 +103,7 @@ var connectFour = {
       }
     }
 
-    for (var i = row + 1; i < 8; i++) {
+    for (var i = row + 1; i < 7; i++) {
       if (playerMoves.includes(`${col}.${i}`)) {
         top++;
       } else {
@@ -105,6 +112,70 @@ var connectFour = {
     }
 
     var idx = bottom + top;
+
+    return idx;
+  },
+  checkMovesBottomLeftTopRight: function (space, playerMoves) {
+    var col = this.curCol(space);
+    var row = this.curRow(space);
+
+    var bottomLeft = 0;
+    var topRight = 0;
+
+    var x = row-1;
+    var y = row+1;
+
+    for (var i = col - 1; i > 0; i--) {
+      if (playerMoves.includes(`${i}.${x}`)) {
+        bottomLeft++;
+      } else {
+        break;
+      }
+      x--;
+    }
+
+    for (var i = col + 1; i < 8; i++) {
+      if (playerMoves.includes(`${i}.${y}`)) {
+        topRight++;
+      } else {
+        break;
+      }
+      y++;
+    }
+
+    var idx = bottomLeft + topRight;
+
+    return idx;
+  },
+  checkMovesBottomRightTopLeft: function (space, playerMoves) {
+    var col = this.curCol(space);
+    var row = this.curRow(space);
+
+    var bottomRight = 0;
+    var topLeft = 0;
+
+    var x = row-1;
+    var y = row+1;
+
+    for (var i = col + 1; i < 8; i++) {
+      if (playerMoves.includes(`${i}.${x}`)) {
+        bottomRight++;
+      } else {
+        break;
+      }
+      x--;
+    }
+
+    for (var i = col - 1; i > 0; i--) {
+      if (playerMoves.includes(`${i}.${y}`)) {
+        topLeft++;
+      } else {
+        break;
+      }
+      y++;
+    }
+
+    var idx = bottomRight + topLeft;
 
     return idx;
   }
